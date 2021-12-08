761c2caa0f7f0bd3f0f99a638a1568eba733bc3d
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2021-12-08 10:20:05 -0800

    Do not run tests with old gradle when java 17 is used

--- src/test/groovy/nebula/plugin/responsible/OlderGradleNebulaIntegTestPluginLauncherSpec.groovy
+++ src/test/groovy/nebula/plugin/responsible/OlderGradleNebulaIntegTestPluginLauncherSpec.groovy
@@ -3,15 +3,17 @@ package nebula.plugin.responsible
 import nebula.test.functional.ExecutionResult
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import spock.lang.IgnoreIf
 
 /**
  * Runs Gradle Launcher style integration Spock tests on the NebulaIntegTestPlugin class
  */
+@IgnoreIf({ jvm.isJava17() })
 class OlderGradleNebulaIntegTestPluginLauncherSpec extends AbstractNebulaIntegTestPluginLauncherSpec {
     def setup() {
         gradleVersion = '6.9'
     }
-    
+
     @Override
     Class<Plugin<Project>> getPluginClass() {
         return NebulaIntegTestPlugin.class
