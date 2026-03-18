2698e907a34435147f6b3a8ba2b5fcf174ae8e91
John Burns <wakingrufus@gmail.com>
2026-03-18 10:30:01 -0500

    improve tests for integTest standalone plugin
    verify CC-compatible

---
+++ src/test/kotlin/nebula/project/NebulaIntegTestStandalonePluginTest.kt
@@ -0,0 +1,74 @@
+package nebula.project
+
+import nebula.test.dsl.*
+import nebula.test.dsl.TestKitAssertions.assertThat
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.io.TempDir
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import java.io.File
+
+internal class NebulaIntegTestStandalonePluginTest {
+    @TempDir
+    lateinit var projectDir: File
+
+    @ParameterizedTest
+    @EnumSource(SupportedGradleVersion::class)
+    fun test(gradle: SupportedGradleVersion) {
+        val runner = testProject(projectDir) {
+            properties {
+                buildCache(true)
+                configurationCache(true)
+            }
+            rootProject {
+                plugins {
+                    id("java-library")
+                    id("com.netflix.nebula.integtest-standalone")
+                }
+                repositories {
+                    mavenCentral()
+                }
+                dependencies("""testImplementation("junit:junit:latest.release")""")
+                src {
+                    sourceSet("integTest") {
+                        junit4Test()
+                        language("resources", "integTest.properties", "")
+                    }
+                }
+            }
+        }
+
+        val checkResult = runner.run("check") {
+            withGradleVersion(gradle.version)
+        }
+        assertThat(checkResult)
+            .hasNoMutableStateWarnings()
+            .hasNoDeprecationWarnings()
+        assertThat(checkResult.task(":test"))
+            .hasOutcome(TaskOutcome.NO_SOURCE)
+        assertThat(checkResult.task(":integrationTest"))
+            .`as`("check does not depend on integrationTest")
+            .isNull()
+        val integrationTestResult = runner.run("integrationTest") {
+            withGradleVersion(gradle.version)
+        }
+        assertThat(integrationTestResult)
+            .hasNoMutableStateWarnings()
+            .hasNoDeprecationWarnings()
+        assertThat(integrationTestResult.task(":integrationTest"))
+            .`as`("check depends on integrationTest")
+            .hasOutcome(TaskOutcome.SUCCESS, TaskOutcome.FROM_CACHE)
+        assertThat(projectDir.resolve("build/classes/java/integTest/nebula/HelloWorldTest.class"))
+            .`as`("compiles integration test classes")
+            .exists()
+        assertThat(projectDir.resolve("build/resources/integTest/integTest.properties"))
+            .`as`("copies integTest resources")
+            .exists()
+        assertThat(projectDir.resolve("build/integTest-results/TEST-nebula.HelloWorldTest.xml"))
+            .`as`("produces integTest xml test report")
+            .exists()
+        assertThat(projectDir.resolve("build/reports/integTest/index.html"))
+            .`as`("produces integTest html test report")
+            .exists()
+    }
+}
